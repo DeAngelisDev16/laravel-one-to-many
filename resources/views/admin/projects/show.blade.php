9c7256e3786d3a9777939c82399209fd3cac14f9
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-28 17:52:39 +0100

    Show project type on project card in show.blade

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,14 +5,16 @@
     <div class="card p-3">
         <h2>{{$project->title}}</h2>
         <img src="{{asset('storage/'. $project->image)}}" alt="placeholder">
-        <pre>{{$project->author}}</pre>
+        <pre>AUTHOR:{{$project->author}}</pre>
+        <pre>TYPE:{{$project->type->name}}</pre>
         <p>{{$project->description}}</p>
-
-
     </div>
-
-
-    
-    
 </div>
 @endsection
+
+
+
+
+
+    
+    
