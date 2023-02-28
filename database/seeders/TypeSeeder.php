aea4acebc146b3d65d6917283ffd3069a1dbaedb
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-28 16:18:25 +0100

    create type seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,20 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
-class DatabaseSeeder extends Seeder
+class TypeSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-
-        $this->call([
-            TypeSeeder::class,
-            ProjectTableSeeder::class
-        ]);
+        //
     }
 }
